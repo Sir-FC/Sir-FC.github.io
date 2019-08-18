6d5ce4986b4a859d7bcc29839d641633df23336f
Sir-FC <zyzzbai2011@gmail.com>
2019-08-18 18:39:50 +0800

    Site updated: 2019-08-18 18:39:43

---
+++ dist/music.js
@@ -0,0 +1,9 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: '午后红茶',
+        artist: '张国荣',
+        url: 'http://www.ytmp3.cn/down/52632.mp3',
+        cover: 'http://pic.xiami.net/images/album/img85/539685/58121522539686.jpg''
+    }]
+});

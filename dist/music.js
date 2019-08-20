8c89b8725093deedbc0987bbd250c6eaa0297b3a
Sir-FC <zyzzbai2011@gmail.com>
2019-08-21 02:30:41 +0800

    Site updated: 2019-08-21 02:30:39

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: '风筝误',
+        artist: '刘珂矣',
+        url: 'https://up.mcyt.net/?down/46644.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      }
+    ]
+});

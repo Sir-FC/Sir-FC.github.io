b0a6e737a2bbbc37d95d3c7b9631540a0cb56f5d
Sir-FC <zyzzbai2011@gmail.com>
2019-08-21 02:51:56 +0800

    Site updated: 2019-08-21 02:51:55

--- dist/music.js
+++ dist/music.js
@@ -6,20 +6,21 @@ const ap = new APlayer({
       {
         name: "小城故事",
         artist: '邓丽君',
-        url: 'http://www.ytmp3.cn/down/56704.mp3',
-        cover: 'http://pic.xiami.net/images/album/img83/1483/4591621365228685.jpg',
+        url: 'https://5.9.115.214/h5ai/zyzzbai2011/user.root/qbittorrent/04.flac',
+        cover: 'https://pic.xiami.net/images/album/img83/1483/4591621365228685.jpg',
       },
       {
         name: '我只在乎你',
         artist: '邓丽君',
-        url: 'http://www.ytmp3.cn/down/56793.mp3',
-        cover: 'http://pic.xiami.net/images/album/img83/1483/4357371367303231.jpg',
+        url: 'https://5.9.115.214/h5ai/zyzzbai2011/user.root/qbittorrent/05.flac',
+        cover: 'https://pic.xiami.net/images/album/img83/1483/4357371367303231.jpg',
       },
       {
         name: '甜蜜蜜',
         artist: '邓丽君',
-        url: 'http://www.ytmp3.cn/down/50116.mp3',
-        cover: 'http://pic.xiami.net/images/album/imgs/629/203/a606b0fd_488629_1533301420651.jpg',
+        url: 'https://5.9.115.214/h5ai/zyzzbai2011/user.root/qbittorrent/06.flac',
+        cover: 'https://pic.xiami.net/images/album/imgs/629/203/a606b0fd_488629_1533301420651.jpg',
       }
     ]
 });
+

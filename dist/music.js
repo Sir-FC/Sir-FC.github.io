534b5c38301083dad75fb911b231227e02fc3dad
Sir-FC <zyzzbai2011@gmail.com>
2019-08-21 02:38:06 +0800

    Site updated: 2019-08-21 02:38:05

--- dist/music.js
+++ dist/music.js
@@ -1,13 +1,10 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
-    audio: [
-      {
+    audio: [{
         name: '小城故事',
         artist: '邓丽君,
         url: 'http://www.ytmp3.cn/down/56704.mp3',
         cover: 'http://pic.xiami.net/images/album/img83/1483/4591621365228685.jpg',
-      }
-    ]
+      }]
 });
